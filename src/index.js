13ebf138f95abf0b8ce6884174b3990d67bed1a9
pre2612 <>
2021-08-25 00:02:21 -0400

    Created Store and added to Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import middleware from './middleware'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App/App';
 
+const store = createStore(reducers, middleware);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
